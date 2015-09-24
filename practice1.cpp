6bcb16f71a4fa164d694a541cc935ff0105af7cc
Joe Welch <jwelch@hartnell.edu>
2015-09-24 09:42:09 -0700

    Practice programs for CSS-1 Exam 1

---
+++ practice1.cpp
@@ -0,0 +1,24 @@
+// Practice Program 1
+//
+// Write a program which implements and tests the following requirements:
+
+// Write and test the isPassed(double valA, double valB) function which averages the two inputs and 
+// returns true if the average is between 80 to 100 (inclusive) and returns false if the function is less than 80 
+// or more than 100. At a minimum, the following tests must be used and passed [remember 0 is false and 1 
+// is true];
+
+// 	assert(isPassed(85.0, 78.0) == 1);
+// 	assert(isPassed(180.0, 100.2) == 0);
+// 	assert(isPassed(-90, 68.0) == 0);
+//  *************************************************************************
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+	return 0;
+}
---
+++ practice2.cpp
@@ -0,0 +1,25 @@
+/*
+Practice Program 2
+
+Write a program which implements and tests the following requirements:
+
+Write and test the addUp(int valA) function which calculates and returns the sum of the integers from 1 to valA (inclusive). If a negative number is passed to the function, the function should return -999. At a minimum, the following tests must be used and passed:
+	
+	assert(addUp(-4) == -999);
+	assert(addUp(1) == 1);
+	assert(addUp(50) == ?? );
+
+___________________________________________________________________
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+	return 0;
+}
